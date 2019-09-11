c90a707baaec079aeda842447c61e863bc05fb9c
Mark McLaughlin <mark@mclaughlin.me.uk>
2019-09-10 22:18:25 -0700

    feat: Generate league data from dato

---
+++ src/_data/leagues.js
@@ -0,0 +1,44 @@
+const fetch = require("node-fetch");
+const token = process.env.DATOCMS_TOKEN;
+
+async function getAllLeagues() {
+  const recordsPerQuery = 100;
+  let recordsToSkip = 0;
+  let makeNewQuery = true;
+  let leagues = [];
+
+  try {
+    const dato = await fetch("https://graphql.datocms.com/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        accept: "application/json",
+        Authorization: `Bearer ${token}`
+      },
+      body: JSON.stringify({
+        query: `{
+          allLeagues(
+            first: ${recordsPerQuery},
+            skip: ${recordsToSkip},
+            orderBy: _createdAt_DESC,
+            filter: {
+              _status: {eq: published}
+            }
+          )
+          {
+            id
+            name
+            sponsor
+          }
+        }`
+      })
+    });
+    const response = await dato.json();
+    leagues = response.data.allLeagues;
+    return leagues;
+  } catch (error) {
+    throw new Error(error);
+  }
+}
+
+module.exports = getAllLeagues;
